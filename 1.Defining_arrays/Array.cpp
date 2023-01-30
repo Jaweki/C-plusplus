e3b42030164f342e22dfcf907c0276f71d55e082
Jack Weru Kioni <werukioni@gmail.com>
2023-01-30 23:39:37 +0300

    Add files via upload

---
+++ 1.Defining_arrays/Array.cpp
@@ -0,0 +1,22 @@
+// array.cpp
+// To input numbers into an array and output after.
+// ----------------------------------------------------
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+	const int MAXCNT = 10; // Constant
+	float arr[MAXCNT], x; // Array, temp. float variable
+	int i, cnt; // Index, quantity
+	cout << "Enter up to 10 numbers \n"
+		<< "(Quit with a letter):" << endl;
+	for (i = 0; i < MAXCNT && cin >> x; ++i)
+		arr[i] = x;
+	cnt = i;
+	cout << "The given numbers:\n" << endl;
+	for (i = 0; i < cnt; ++i)
+		cout << setw(10) << arr[i];
+	cout << endl;
+	return 0;
+}
